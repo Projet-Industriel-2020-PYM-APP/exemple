cfaea482eab7daef988c3e2e5133063f8425092f
Franck Sebelon <weonx47.1@gmail.com>
2019-11-22 16:52:51 +0100

    version du meilleur

--- src/main.cpp
+++ src/main.cpp
@@ -1,57 +1,9 @@
-#include "batiment.h"
-#include "liste_salles.h"
-#include "salle_cours.h"
-#include "salle_info.h"
+#include "Salle.hpp"
 
-#include <fstream>
-
-constexpr char FILENAME[] = "salles.txt";
-
-void lireFicSalle(const std::string& nomFic, ListeSalles* liste);
-
-int main() {
-  ListeSalles listeSalles;
-  lireFicSalle(FILENAME, &listeSalles);
-  listeSalles.show();
-
-  Batiment batiment(listeSalles);
-  batiment.positionner(0, 0, 0);
-  batiment.positionner(1, 0, 20);
-  return 0;
-}
-
-void lireFicSalle(const std::string& nomFic, ListeSalles* liste) {
-  std::ifstream fileStream(nomFic);
-  std::cout << "Parsing " << nomFic << std::endl;
-
-  if (fileStream.is_open()) {
-    while (true) {
-      // Data
-      int longueur, largeur;
-      long nbPlaces;
-      std::string nom;
-
-      // Parse
-      std::string type;
-      fileStream >> type >> nom >> longueur >> largeur >> nbPlaces;
-      if (fileStream.fail()) break;
-      if (type == "SALLE_COURS") {
-        auto newSalle = new SalleCours(nom, longueur, largeur, nbPlaces, false);
-        liste->ajouter(newSalle);
-      } else if (type == "AMPHI") {
-        auto newSalle = new SalleCours(nom, longueur, largeur, nbPlaces, true);
-        liste->ajouter(newSalle);
-      } else if (type == "SALLE_INFO") {
-        long nbPostesInstalles;
-        fileStream >> nbPostesInstalles;
-        auto newSalle =
-            new SalleInfo(nom, longueur, largeur, nbPlaces, nbPostesInstalles);
-        liste->ajouter(newSalle);
-      } else {
-        std::cout << "Error : one line escaped." << std::endl;
-      }
-    }
-  } else {
-    std::cout << "Error" << std::endl;
-  }
+int main()
+{
+	SalleCours s1("C003", 12, 10, 35, 1);
+	s1.afficher();
+	
+	return 0;
 }
--- src/salle.cpp
+++ src/salle.cpp
@@ -1,10 +1,55 @@
-#include "salle.h"
+#include "Salle.hpp"
 
-#include <iostream>
 
-void Salle::show() const {
-  std::cout << "Salle " << _nom << std::endl
-            << "Longueur : " << _longueur << std::endl
-            << "Largeur : " << _largeur << std::endl
-            << "Nb. de places : " << _nbPlaces << std::endl;
+const void Salle::afficher()
+{
+	cout<<"Salle "<<s_nom<<" : "<<endl;
+	cout<<"  dimensions : "<<s_largeur<<" X "<<s_longueur<<endl;
+	cout<<"  capacite : "<<s_capacite<<endl;
 }
+
+
+const void SalleCours::afficher()
+{
+	Salle::afficher();
+	if (s_amphi) 
+	{
+		cout<<"  Cette salle est un amphi"<<endl;
+	}
+}
+
+const void SalleInfo::afficher()
+{
+	Salle::afficher();
+	if (s_postes) 
+	{
+		cout<<"  Cette salle contient "<<s_postes<<" de travail"<<endl;
+	}
+	else
+	{
+		cout<<"  Cette salle est destinee aux ordinateurs portables"<<endl;
+	}
+}
+
+void ListeSalles::ajouter(Salle* salle)
+{
+	bool existe = 0;
+	for (int i=0; i<l_compteur; i++)
+	{
+		if (salle->getNom()==l_tableau[i]->getNom())
+		{
+			existe = 1;
+		}
+	}
+	if (!existe)
+	{
+		l_compteur++;
+		l_tableau[l_compteur] = salle;
+	}
+}
+	
+	
+		
+		
+		
+		
